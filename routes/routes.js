1a5ac3190a83bb214008b7c90661c71670fd6ce3
Teo Wei Shen <tws4793@gmail.com>
2018-10-16 21:51:21 +0800

    Modify Play store route

--- routes/routes.js
+++ routes/routes.js
@@ -1,17 +1,17 @@
+const gplay = require('google-play-scraper')
+
 const router = (app) => {
-    app.get('/', function (req, res) {
+    app.get('/', (req, res) => {
         res.send('Hello World')
     })
 
-    app.get('/reviews', function (req, res) {
-        const gplay = require('google-play-scraper')
-
+    app.get('/play/reviews', (req, res) => {
         gplay.reviews({
             appId: req.query.id,
             lang: req.query.lang,
             page: req.query.page,
             throttle: 10,
-        }).then((response) => {
+        }).then(response => {
             res.send(response)
         })
     })
